ecea8a6014c3fbf4c538e41757b999a5782d7de8
sp-yduck <sudo20.t.ab@gmail.com>
2025-01-11 02:34:21 +0000

    publish package

--- packages/react-tailwind-kanban/src/components/kanban/Board.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const Kanban: import("react").ForwardRefExoticComponent<import("react").HTMLAttributes<HTMLDivElement> & import("react").RefAttributes<HTMLDivElement>>;
-export declare const KanbanHeader: import("react").ForwardRefExoticComponent<import("react").HTMLAttributes<HTMLDivElement> & import("react").RefAttributes<HTMLDivElement>>;
-export declare const KanbanTitle: import("react").ForwardRefExoticComponent<import("react").HTMLAttributes<HTMLDivElement> & import("react").RefAttributes<HTMLDivElement>>;
-export declare const KanbanContent: import("react").ForwardRefExoticComponent<import("react").HTMLAttributes<HTMLDivElement> & import("react").RefAttributes<HTMLDivElement>>;
